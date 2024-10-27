19b5bbbb85a66b770445921cd928a4a05abf63b1
zt <57047029+ztexdee@users.noreply.github.com>
2024-10-27 19:31:26 +0300

    initialized project

---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import { fileURLToPath, URL } from 'node:url'
+import  { resolve } from 'path'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    react()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'zt-vue-library',
+      fileName: 'index',
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: [
+        'vue'
+      ],
+      
+       output: {
+        // this for importing css into index.js 
+        intro: 'import "./style.css";',
+      //   // TODO: check if necessary
+      //   globals: {
+      //     // TODO: should it be upper case?
+      //     vue: 'vue',
+      //   }
+      }
+    }
+  }
+})
